bbf20b02d005077adf902e793208c6d57d507ec4
nova_ <nova_09@mail.ru>
2023-02-01 17:46:25 +1000

    Refactoring

--- app/src/main/java/z/nova/rifmobolgame/data/singlerepository/GameRepo.kt
+++ app/src/main/java/z/nova/rifmobolgame/data/singlerepository/GameRepo.kt
@@ -1,11 +1,8 @@
 package z.nova.rifmobolgame.data.singlerepository
 
-import z.nova.rifmobolgame.data.model.DialogText
 import z.nova.rifmobolgame.data.model.RoundInfo
 import z.nova.rifmobolgame.data.model.RoundMusic
 import z.nova.rifmobolgame.data.model.TextLevel
-import z.nova.rifmobolgame.data.model.multi.DialogMultiText
-import z.nova.rifmobolgame.data.model.multi.RoundMultiMusic
 
 interface GameRepo {
     fun getTextLvlInfo(): List<TextLevel>
